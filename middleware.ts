56669a0b7b61c7ed81b7b33e87868ec9f8e2afd8
brennanlazzara <brennan.lazzara@carters.com>
2025-12-07 03:13:38 +0100

    fix: temporarily disable auth middleware for Vercel demo deployment

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,5 @@
-// middleware.ts
-// Temporarily disabled auth middleware for Vercel demo deployment.
-// All auth is handled in layouts & server components instead.
+export { auth as middleware } from './auth'
 
-// If you want to re-enable later, restore the `export { auth as middleware } from "./auth"` form.
-
-// export { auth as middleware } from "./auth";
-
-// export const config = {
-//   matcher: ["/((?!api|_next/static|_next/image|favicon.ico|.*\\.png$).*)"],
-// };
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|.*\\.png$).*)'],
+}
